d4e57b5b9ba9641410fbefbdb82b0c8d6417af02
ezekielcodes01 <ekajiezekiel003@gmail.com>
2026-01-31 22:42:46 +0000

    initial commit

---
+++ javascripts/amazon.js
@@ -0,0 +1,16 @@
+const products = [
+ {
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: { stars: 4.5, count: 87 },
+  priceCent: 1090,
+ },
+ {
+  image: 'images/products/intermediate-composite-basketball.jpg',
+   name: 'Intermediate Size Basketball', rating: {
+     stars: 4,
+     count: 127
+   },
+   priceCent: 2095
+ },
+];
