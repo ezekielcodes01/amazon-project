27bd2d128dbf3dd2068b56bfc8e6e98b24e62736
ezekielcodes01 <ekajiezekiel003@gmail.com>
2026-02-02 23:11:08 +0000

    atc commit

--- javascripts/amazon.js
+++ javascripts/amazon.js
@@ -43,7 +43,7 @@ products.forEach((product) => {
                 Added
               </div>
 
-              <button class="add-to-cart-button button-primary">
+              <button class="add-to-cart-button button-primary js-add-to-cart">
                 Add to Cart
               </button>
             </div>
@@ -51,3 +51,10 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      
+    })
+  })
